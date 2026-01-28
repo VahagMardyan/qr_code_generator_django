84794cd2394efd285b151f5ab4f1812f799561bd
VahagMardyan <mardyan2003@gmail.com>
2026-01-28 08:37:16 +0400

    Added allowed hosts

--- qr_code_maker/settings.py
+++ qr_code_maker/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://qr-code-generator-django.onrender.com/",
+    "localhost",
+    "127.0.0.1"
+]
 
 # Application definition
 
